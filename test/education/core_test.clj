7479013699254ced4ecb93ced23a993b0d6ec57c
Roman Rudakov <rrudakov@protonmail.com>
2020-06-06 17:50:34 +0200

    Add tests for core namespace

---
+++ test/education/core_test.clj
@@ -0,0 +1,14 @@
+(ns education.core-test
+  (:require [clojure.test :refer :all]
+            [com.stuartsierra.component :as component]
+            [education.core :as sut]
+            [education.system :refer [api-system]]
+            [spy.core :as spy]))
+
+(deftest main-test
+  (testing "Test application start system with prod profile"
+    (with-redefs [component/start (spy/stub)
+                  api-system (spy/stub)]
+      (sut/-main)
+      (is (spy/called-once? component/start))
+      (is (spy/called-once-with? api-system :prod)))))
