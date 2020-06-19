66df8d9baf1f6ce511e5941cec8070d56ab77ec6
Roman Rudakov <rrudakov@protonmail.com>
2020-06-19 18:15:24 +0200

    Add unit tests for roles API

---
+++ test/education/http/endpoints/roles_test.clj
@@ -0,0 +1,41 @@
+(ns education.http.endpoints.roles-test
+  (:require [clojure.test :refer [deftest is testing]]
+            [education.database.roles :as rolesdb]
+            [education.http.constants :refer :all]
+            [education.http.endpoints.roles :as sut]
+            [education.http.endpoints.test-app :refer :all]
+            [education.test-data :refer :all]
+            [ring.mock.request :as mock]
+            [spy.core :as spy]))
+
+(deftest roles-test
+  (testing "Test GET /roles with admin role"
+    (with-redefs [rolesdb/get-all-roles (spy/stub db-all-roles)]
+      (let [app      (test-api-routes-with-auth (spy/spy))
+            response (app (-> (mock/request :get "/api/roles")
+                              (mock/header :authorization (test-auth-token #{:admin}))))
+            body     (parse-body (:body response))]
+        (is (= 200 (:status response)))
+        (is (= [{:id 3, :name "guest"}
+                {:id 2, :name "moderator"}
+                {:id 1, :name "admin"}] body))
+        (is (spy/called-once-with? rolesdb/get-all-roles nil)))))
+
+  (testing "Test GET /roles with moderator role"
+    (with-redefs [rolesdb/get-all-roles (spy/spy)]
+      (let [app      (test-api-routes-with-auth (spy/spy))
+            response (app (-> (mock/request :get "/api/roles")
+                              (mock/header :authorization (test-auth-token #{:moderator}))))
+            body     (parse-body (:body response))]
+        (is (= 403 (:status response)))
+        (is (= {:message no-access-error-message} body))
+        (is (spy/not-called? rolesdb/get-all-roles)))))
+
+  (testing "Test GET /roles without authorization token"
+    (with-redefs [rolesdb/get-all-roles (spy/spy)]
+      (let [app      (test-api-routes-with-auth (spy/spy))
+            response (app (-> (mock/request :get "/api/roles")))
+            body     (parse-body (:body response))]
+        (is (= 401 (:status response)))
+        (is (= {:message not-authorized-error-message} body))
+        (is (spy/not-called? rolesdb/get-all-roles))))))
