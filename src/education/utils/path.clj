2cb3862455e86f7acfbd63a7e6a87e379d4197d8
Roman Rudakov <rrudakov@protonmail.com>
2020-10-27 15:34:50 +0100

    Add unit tests for utils and fix some issues

--- src/education/utils/path.clj
+++ src/education/utils/path.clj
@@ -3,15 +3,17 @@
 
 (defn- strip-prefix-slash
   [s]
-  (if (str/starts-with? s "/")
-    (subs s 1)
-    s))
+  (let [s (str/trim s)]
+    (if (str/starts-with? s "/")
+      (subs s 1)
+      s)))
 
 (defn- strip-suffix-slash
   [s]
-  (if (str/ends-with? s "/")
-    (subs s 0 (dec (count s)))
-    s))
+  (let [s (str/trim s)]
+    (if (str/ends-with? s "/")
+      (subs s 0 (dec (count s)))
+      s)))
 
 (defn- join-two-paths
   [a b]
@@ -20,4 +22,6 @@
 
 (defn join
   [base & args]
-  (str/join "/" [(strip-suffix-slash base) (reduce join-two-paths args)]))
+  (if args
+    (str/join "/" [(strip-suffix-slash base) (reduce join-two-paths args)])
+    (strip-suffix-slash base)))
---
+++ test/education/utils/maps_test.clj
@@ -0,0 +1,27 @@
+(ns education.utils.maps-test
+  (:require [education.utils.maps :as sut]
+            [clojure.test :refer [testing deftest is]]))
+
+(deftest unqualify-keywork-test
+  (testing "Test unqualify keyword with namespace"
+    (is (= [:id 1] (sut/unqualify-keyword [:lessons/id 1]))))
+
+  (testing "Test unqualify keyword without namespace"
+    (is (= [:title "Some title"] (sut/unqualify-keyword [:title "Some title"])))))
+
+(deftest unqualify-map-test
+  (testing "Test unqualify map with namespaces"
+    (is (= {:id       1
+            :title    "Some title"
+            :subtitle "Some subtitle"}
+           (sut/unqualify-map {:lessons/id       1
+                               :lessons/title    "Some title"
+                               :lessons/subtitle "Some subtitle"}))))
+
+  (testing "Test unqualify map without namespaces"
+    (is (= {:id       1
+            :title    "Some title"
+            :subtitle "Some subtitle"}
+           (sut/unqualify-map {:id       1
+                               :title    "Some title"
+                               :subtitle "Some subtitle"})))))
