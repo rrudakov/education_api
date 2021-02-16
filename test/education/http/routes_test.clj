946fa7880ff2f28965cdd2b9427b17cd65ea9e31
Roman Rudakov <rrudakov@protonmail.com>
2021-02-16 13:42:15 +0100

    [#40] Fix response-validation-handler-test

--- test/education/http/routes_test.clj
+++ test/education/http/routes_test.clj
@@ -1,30 +1,32 @@
 (ns education.http.routes-test
-  (:require [clojure.test :refer [deftest is testing]]
+  (:require [clojure.spec.alpha :as s]
+            [clojure.test :refer [deftest is testing]]
             [education.http.constants :as const]
-            [education.http.endpoints.presentations :as presentations-endpoint]
+            [education.http.endpoints.dresses :as dresses-endpoint]
             [education.http.endpoints.test-app :as test-app]
+            [education.specs.common :as spec]
             [ring.mock.request :as mock]
-            [spy.core :as spy]
-            [clojure.spec.alpha :as s]
-            [education.specs.common :as spec])
+            [spy.core :as spy])
   (:import java.time.Instant))
 
-(def ^:private presentation-response-invalid
-  "Invalid API presentation response."
+(def ^:private dress-response-invalid
+  "Invalid API dress response."
   {:id          22
    :title       "Title"
-   :url         "invalid url"
-   :description "Some description"
+   :description "Description"
+   :size        38
+   :pictures    ["invalid url"]
+   :price       "22.7"
    :created_on  (Instant/now)
    :updated_on  (Instant/now)})
 
 (deftest response-validation-handler-test
   (testing "Test exception handler for invalid response body"
-    (with-redefs [presentations-endpoint/get-presentation-by-id-handler (spy/stub presentation-response-invalid)]
+    (with-redefs [dresses-endpoint/get-dress-by-id-handler (spy/stub dress-response-invalid)]
       (let [app      (test-app/api-routes-with-auth)
-            response (app (mock/request :get "/api/presentations/22"))
+            response (app (mock/request :get "/api/dresses/22"))
             body     (test-app/parse-body (:body response))]
         (is (= 500 (:status response)))
         (is (= {:message const/server-error-message
-                :details (s/explain-str ::spec/presentation-response presentation-response-invalid)}
+                :details (s/explain-str ::spec/dress-response dress-response-invalid)}
                body))))))
